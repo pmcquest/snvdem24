1982aac827b2d2dbb78f3e2055a41c53ef643fb5
Patrick McQuestion <pmcquest@nd.edu>
2024-01-15 18:42:06 -0500

    Reviewed the correlations and added some details to first scatterplot.

--- scripts/CreateColombiaSN_010924.R
+++ scripts/CreateColombiaSN_010924.R
@@ -5,9 +5,11 @@ rm(list = ls())
 
 # load needed packages
 library(tidyverse)
+library(vdemdata)
 
-# Create a data frame of the "full + others" data set
-v13 <- read_dta("C:/Users/mcoppedg/Dropbox/MC/VDemFiles/Archive/V13/V-Dem-CY-Full+Others/V-Dem-CY-Full+Others-v13.dta")
+# Create a data frame of version 13 dataset:
+v13 <- vdem
+# this df appears to be the same as MC's "full + others" data set: read_dta("C:/Users/mcoppedg/Dropbox/MC/VDemFiles/Archive/V13/V-Dem-CY-Full+Others/V-Dem-CY-Full+Others-v13.dta")
 
 # Filter to just Colombia since 1899:
 v13_col <- filter(v13, country_name=="Colombia" & year > 1899)
@@ -53,15 +55,21 @@ write.csv(v13_col_sn, file = "v13_col_sn.csv")
 
 # Ways to explore relationships. Examples using v2elsnlfc_*
 
-
 # Make a scatterplot to check the relationship between rural (0) and urban (1)
 # (by default, for all years)
 ggplot(v13_col_sn, aes(x =  v2elsnlfc_0, y =  v2elsnlfc_1)) +
-  geom_point()
+  geom_point() +
+  geom_text(aes(label = year), vjust = 1.5, hjust = 0.5) + 
+  labs(title = "Scatterplot of less free and fair subnational elections: rural vs urban",
+       x = "Rural (proportion)",
+       y = "Urban (proportion)")
+# PM// Note that subnational elections in Colombia began in 1988, so points are 
+# likely limited to the period since then.
 
 # Correlate variables that we hope are opposite poles
 rich_poor <- select(v13_col_sn, c(v2elsnlfc_2, v2elsnlfc_3))
 cor(rich_poor, use = "pairwise")
+# PM// far from correlated... good sign.
 
 # Correlate all variables (except *_16 and beyond, which are all NA)
 allvars <- select(v13_col_sn, v2elsnlfc_0:v2elsnlfc_15)
@@ -71,8 +79,15 @@ cor(allvars, use = "pairwise")
 # I've inserted labels to make it easier to read.
 install.packages("GGally")
 library(GGally)
-ggpairs(allvars, columns = c("v2elsnlfc_0", "v2elsnlfc_1", "v2elsnlfc_2", "v2elsnlfc_3", 'v2elsnlfc_4', "v2elsnlfc_5", "v2elsnlfc_6", 'v2elsnlfc_7', 'v2elsnlfc_8', "v2elsnlfc_9", "v2elsnlfc_10", "v2elsnlfc_11", "v2elsnlfc_12", "v2elsnlfc_13", "v2elsnlfc_14", "v2elsnlfc_15"),
-        columnLabels = c("rural", "urban", "poor", "rich", 'capital', "not_cap", "N", 'S', 'E', "W", "unrest", "illicit", "sparse", "remote", "indig", "par_strong"),
+ggpairs(allvars, columns = c("v2elsnlfc_0", "v2elsnlfc_1", "v2elsnlfc_2", 
+                             "v2elsnlfc_3", 'v2elsnlfc_4', "v2elsnlfc_5", 
+                             "v2elsnlfc_6", 'v2elsnlfc_7', 'v2elsnlfc_8', 
+                             "v2elsnlfc_9", "v2elsnlfc_10", "v2elsnlfc_11", 
+                             "v2elsnlfc_12", "v2elsnlfc_13", "v2elsnlfc_14", 
+                             "v2elsnlfc_15"),
+        columnLabels = c("rural", "urban", "poor", "rich", 'capital', 
+                         "not_cap", "N", 'S', 'E', "W", "unrest", "illicit", 
+                         "sparse", "remote", "indig", "par_strong"),
         switch = "both",
         mapping = ggplot2::aes(alpha = .2), theme(text = element_text(size = 1)),
         title="subnational variables") 
